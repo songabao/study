e10de024d8b1f5102bd9f6163871426a8013b51c
songabao <1919103133@qq.com>
2019-08-06 16:34:15 +0800

    ribbon的生产者

---
+++ microservice-config-server-production-ribbon/src/main/java/com/microservice/service/CityService.java
@@ -0,0 +1,11 @@
+package com.microservice.service;
+
+import com.microservice.domain.City;
+
+/**
+ * @author songabao
+ * @date 2019/6/18 15:52
+ */
+public interface CityService {
+    City getCity(Long id);
+}
