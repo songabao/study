6a91648a5f81d9e51eecd5837891b4639d44a889
songabao <1919103133@qq.com>
2019-08-06 16:25:47 +0800

    ribbon在eureka中的使用

---
+++ microservice-config-server-consums-ribbon-without-eureka/src/main/java/com/microservice/controller/CityController.java
@@ -0,0 +1,43 @@
+package com.microservice.controller;
+
+import com.microservice.domain.City;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author songabao
+ * @date 2019/6/18 16:10
+ */
+@RestController
+public class CityController {
+    private static  final Logger log = LoggerFactory.getLogger(CityController.class);
+
+    @Autowired
+    private RestTemplate restTemplate;
+    @Autowired
+    private LoadBalancerClient loadBalanced;
+
+    @GetMapping(value = "/getCity")
+    public City getCity(@RequestParam("id") Long id){
+        ResponseEntity<City> forObject = restTemplate.getForEntity("Http://microservice-config-server-production-ribbon-without-eureka/getCity?id=" + id, City.class);
+        City city  = forObject.getBody();
+        return city;
+    }
+
+    /**
+     * <b>说明：查看当前选中的节点</b>
+     */
+    @GetMapping(value = "/findUserInstance")
+    public void  findUserInstance(){
+        ServiceInstance choose = this.loadBalanced.choose("microservice-config-server-production-ribbon-without-eureka");
+        log.info("{}：{}",choose.getHost(),choose.getPort());
+    }
+}
